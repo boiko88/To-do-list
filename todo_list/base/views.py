f3af91d74ff6c5cd32e838f223a729f8be8d1a4b
Anton Boiko <bo1ko.ant@yandex.com>
2022-08-19 00:16:53 +0400

    created templates along with todo_list.html document and now tasks are visible

--- todo_list/base/views.py
+++ todo_list/base/views.py
@@ -1,5 +1,7 @@
+from typing import List
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.views.generic.list import ListView
+from .models import Task
 
-def taskList(request):
-    return HttpResponse('To do list seems it works')
+class TaskList(ListView):
+    model = Task

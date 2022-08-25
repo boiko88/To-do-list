3455f1553658d70a4074eab2d5cfea818cbca5b3
Anton Boiko <bo1ko.ant@yandex.com>
2022-08-25 19:56:29 +0300

    added a registered form

--- todo_list/base/views.py
+++ todo_list/base/views.py
@@ -22,18 +22,31 @@ class TaskList(LoginRequiredMixin, ListView):
     model = Task
     context_object_name = 'tasks'
     
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['tasks'] = context['tasks'].filter(user=self.request.user)
+        context['count'] = context['tasks'].filter(complete=False).count()
+        return context
+        
+    
 
 class TaskDetail(LoginRequiredMixin, DetailView):
     model = Task
+    context_object_name = 'task'
+    template_name = 'base/task.html'
     
 class TaskCreate(LoginRequiredMixin, CreateView):
     model = Task
-    fields = '__all__'
+    fields = ['title', 'description', 'complete']
     success_url = reverse_lazy('tasks')
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super(TaskCreate, self).form_valid(form)
     
 class TaskUpdate(LoginRequiredMixin, UpdateView):
     model = Task
-    fields = '__all__'
+    fields = ['title', 'description', 'complete']
     success_url = reverse_lazy('tasks')
     
     
